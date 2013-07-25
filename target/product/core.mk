f9193222e20b10b8c395ff95e81b5c014a1f34bf
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2013-07-25 07:31:47 +0530

    product/core: Install busybox, lrzsz, ffmpeg, x264
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- target/product/core.mk
+++ target/product/core.mk
@@ -31,6 +31,70 @@ PRODUCT_PACKAGES += \
     PacProcessor \
     ProxyHandler \
     SharedStorageBackup \
-    VpnDialogs
+    TelephonyProvider \
+    UserDictionaryProvider \
+    VpnDialogs \
+    atrace \
+    libandroidfw \
+    libaudiopreprocessing \
+    libaudioutils \
+    libbcc \
+    libfilterpack_imageproc \
+    libgabi++ \
+    libkeystore \
+    libmdnssd \
+    libnfc_ndef \
+    libportable \
+    libpowermanager \
+    libspeexresampler \
+    libstagefright_chromium_http \
+    libstagefright_soft_aacdec \
+    libstagefright_soft_aacenc \
+    libstagefright_soft_amrdec \
+    libstagefright_soft_amrnbenc \
+    libstagefright_soft_amrwbenc \
+    libstagefright_soft_flacenc \
+    libstagefright_soft_g711dec \
+    libstagefright_soft_gsmdec \
+    libstagefright_soft_h264dec \
+    libstagefright_soft_h264enc \
+    libstagefright_soft_mp3dec \
+    libstagefright_soft_mpeg4dec \
+    libstagefright_soft_mpeg4enc \
+    libstagefright_soft_rawdec \
+    libstagefright_soft_vorbisdec \
+    libstagefright_soft_vpxdec \
+    libstagefright_soft_vpxenc \
+    libvariablespeed \
+    libwebrtc_audio_preprocessing \
+    mdnsd \
+    mms-common \
+    requestsync \
+    telephony-common \
+    voip-common
+
+# linaro dependencies
+PRODUCT_PACKAGES += \
+    busybox \
+    ffmpeg \
+    lrzsz \
+    wpa_supplicant.conf \
+    x264
+
+# host-only dependencies
+ifeq ($(WITH_HOST_DALVIK),true)
+    PRODUCT_PACKAGES += \
+        apache-xml-hostdex \
+        bouncycastle-hostdex \
+        core-hostdex \
+        libcrypto \
+        libexpat \
+        libicui18n \
+        libicuuc \
+        libjavacore \
+        libssl \
+        libz-host \
+        dalvik
+endif
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
